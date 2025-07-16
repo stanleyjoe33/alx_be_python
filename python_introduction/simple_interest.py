d48d67b4fd97bdf220916645df738994d0daf4b0
Joseph Stanley <stanleyojoseph33@gmail.com>
2025-07-16 20:35:09 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@
 principal = 1000  
 rate = 0.05  
 time = 3  
-I = principal * rate * time
-Print (The simple interest is: "I")
+interest = principal * rate * time
+Print (The simple interest is: "interest")
