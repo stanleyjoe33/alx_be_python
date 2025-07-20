ea568a8965e727591df5a76bf83bf1e8b490a7bc
Joseph Stanley <stanleyojoseph33@gmail.com>
2025-07-20 11:17:47 +0100

    Create  basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+
+# Define variables
+number1 = 10
+number2 = 5
+
+# Perform calculations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print results
+print(f"Sum of {number1} and {number2} is {sum_result}")
+print(f"Difference of {number1} and {number2} is {difference}")
+print(f"Product of {number1} and {number2} is {product}")
