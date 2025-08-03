c53fe5a0a26d49458f2281d01eab0d40ea8cb283
Joseph Stanley <stanleyojoseph33@gmail.com>
2025-08-03 22:35:39 +0100

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,4 +1,4 @@
-import unittest
+ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
@@ -14,7 +14,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test subtraction functionality."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
